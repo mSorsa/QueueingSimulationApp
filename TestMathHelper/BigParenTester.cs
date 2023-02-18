48b36d0854db412d6735fe1529f3bda5ac0b7876
mSorsa <martin.sorsa@gmail.com>
2023-02-18 12:42:19 +0100

    Added tests for Big Parenthesis Calculator.

---
+++ TestMathHelper/BigParenTester.cs
@@ -0,0 +1,24 @@
+﻿using MathematicalHelper;
+
+namespace TestMathHelper
+{
+    public class BigParenTester
+    {
+        private static readonly BigParenCalculator _calculator = new(new FactorialCalculator());
+
+        [Theory]
+        [InlineData(8, 6, 28)]
+        [InlineData(4, 0, 1)]
+        [InlineData(4, 1, 4)]
+        [InlineData(7, 3, 35)]
+        [InlineData(12, 10, 66)]
+        [InlineData(9, 2, 36)]
+        public void CalculateBigParenExpression_ValidInputs_ReturnsExpectedValue(int n, int k, double expected)
+        {
+            // Act
+            var actual = _calculator.CalculateBigParen(n, k);
+            // Assert
+            Assert.Equal(expected: expected, actual: actual);
+        }
+    }
+}
