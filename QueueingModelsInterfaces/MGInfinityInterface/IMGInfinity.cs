3568b7485c7127cd7dafd433c473b5ee5b98e16d
mSorsa <martin.sorsa@gmail.com>
2023-04-16 14:45:17 +0200

    Updated summaries

--- QueueingModelsInterfaces/MGInfinityInterface/IMGInfinity.cs
+++ QueueingModelsInterfaces/MGInfinityInterface/IMGInfinity.cs
@@ -2,11 +2,48 @@
 {
     public interface IMGInfinity
     {
+        /// <summary>
+        /// Probability of empty system
+        /// </summary>
+        /// <param name="lambda"></param>
+        /// <param name="mu"></param>
+        /// <returns></returns>
         double CalculatePZero(double lambda, double mu);
+
+        /// <summary>
+        /// Mean time in system
+        /// </summary>
+        /// <param name="mu"></param>
+        /// <returns></returns>
         double CalculateW(double mu);
+
+        /// <summary>
+        /// Mean time in queue
+        /// </summary>
+        /// <returns></returns>
         double CalculateWq();
+
+        /// <summary>
+        /// Mean number in system
+        /// </summary>
+        /// <param name="lambda"></param>
+        /// <param name="mu"></param>
+        /// <returns></returns>
         double CalculateL(double lambda, double mu);
+
+        /// <summary>
+        /// Mean number in queue
+        /// </summary>
+        /// <returns></returns>
         double CalculateLq();
+
+        /// <summary>
+        /// Probability of n customers in system
+        /// </summary>
+        /// <param name="lambda">Arrival rate</param>
+        /// <param name="mu">Service rate</param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         double CalculatePn(double lambda, double mu, int n);
     }
 }
--- QueueingModelsInterfaces/MMCNInterface/IMMCN.cs
+++ QueueingModelsInterfaces/MMCNInterface/IMMCN.cs
@@ -2,14 +2,55 @@
 {
     public interface IMMCN
     {
+        /// <summary>
+        /// Probability of empty system
+        /// </summary>
+        /// <returns></returns>
         double CalculatePZero();
+
+        /// <summary>
+        /// Probability of N customers
+        /// </summary>
+        /// <returns></returns>
         double CalculatePN();
+
+        /// <summary>
+        /// Effective arrival rate
+        /// </summary>
+        /// <returns>lambdaE</returns>
         double CalculateLambdaEffective();
+
+        /// <summary>
+        /// Mean number in system
+        /// </summary>
+        /// <returns></returns>
         double CalculateL();
+
+        /// <summary>
+        /// Mean number in queue
+        /// </summary>
+        /// <returns></returns>
         double CalculateLQ();
+
+        /// <summary>
+        /// Mean time in queue
+        /// </summary>
+        /// <returns></returns>
         double CalculateWQ();
+
+        /// <summary>
+        /// mean time in system
+        /// </summary>
+        /// <returns></returns>
         double CalculateW();
+
+        /// <summary>
+        /// Utilization
+        /// </summary>
+        /// <returns></returns>
         double CalculateRho();
+
+
         double CalculateCapacityUtilization();
     }
 }
